05640981565042328971c3c6225521c677b419d0
hainguyen10 <74067681+hainguyen10@users.noreply.github.com>
2021-02-02 10:44:36 -0700

    Add files via upload

---
+++ Theme.swift
@@ -0,0 +1,15 @@
+//
+//  Theme.swift
+//  Concentration
+//
+//  Created by Son Nguyen  on 1/11/21.
+//  Copyright © 2021 Hai Nguyen. All rights reserved.
+//
+
+import UIKit
+
+struct Theme {
+    var backgroundColor: UIColor
+    var cardColor: UIColor
+    var cardTitles: [String]
+}
